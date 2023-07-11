c1a6081e013a41fe862364d35e28716eb13d4f6d
gperez2222 <student@bchd.a87c3ffd-bd40-4a64-8399-c05145c40292>
2023-07-11 20:20:38 +0000

    studying for logic

---
+++ broken01/ifixed.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+import re
+
+# A program that prompts a user for two operators and operation (plus or minus)
+# the program then shows the result.
+# The user may enter q to exit the program.
+
+calc1 = 0.0
+calc2 = 0.0
+operation = ""
+
+operators = ["+","-","*","/"]
+
+def raw_input():
+    value = input()
+    #if re.match(r'^[A-z]+', value):
+
+    if value.isalpha():
+        return "q"
+    elif value.isdigit():
+        return float(value)
+    elif value in operators:
+        return value
+    else: 
+        return "q"
+
+while (calc1 != "q" and calc2 != "q"):
+
+    print("\nWhat is the first operator? Or, enter q to quit: ", end="")
+    calc1 = raw_input()
+    if calc1 == "q":
+        print(f"Exiting ..\n")
+        break
+
+    print("\nWhat is the second operator? Or, enter q to quit: ", end="")
+    calc2 = raw_input()
+    if calc2 == "q":
+        print(f"Exiting ..\n")
+        break
+
+    print(f"Enter an operation to perform on the two operators {operators}: ", end="")
+    operation = raw_input()
+
+    if operation == '+':
+        total = calc1 + calc2
+        print(f"\n {str(calc1)}  {operation}  {str(calc2)}  = {total}")
+    elif operation == '-':
+        total = calc1 - calc2
+        print(f"\n {str(calc1)}  {operation}  {str(calc2)}  = {total}")
+    elif operation == '*':
+        total = calc1 * calc2
+        print(f"\n {str(calc1)}  {operation}  {str(calc2)}  = {total}")
+    elif operation == '/':
+        total = calc1 / calc2
+        print(f"\n {str(calc1)}  {operation}  {str(calc2)}  = {total}")
+    else:
+        print("\n Not a valid entry. Restarting...")
+

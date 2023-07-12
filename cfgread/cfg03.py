a6bc5566fce17514ec062a78e2f6128310b47a6a
gperez2222 <student@bchd.a87c3ffd-bd40-4a64-8399-c05145c40292>
2023-07-12 19:37:57 +0000

    learning about functions

--- cfgread/cfg03.py
+++ cfgread/cfg03.py
@@ -2,6 +2,7 @@
 
 ## create file object in "r"ead mode
 
+cnt = 0
 newconfiglist = []
 
 with open("vlanconfig.cfg", "r") as configfile:
@@ -13,11 +14,16 @@ with open("vlanconfig.cfg", "r") as configfile:
     for line in configlist:
         nline = line.strip("\n")
         print(nline)
+        if "vlan" in nline:
+            cnt += 1
         newconfiglist.append(nline)
 
 ## file was just auto closed (no more indenting)
 
 ## each item of the list now has the "\n" characters back
-print(configlist)
+#print(configlist)
+print("The number of lines: ", len(configlist))
+print("The number of lines: ", len(newconfiglist))
+print(f"Total lines in the file: {cnt}")
 print(newconfiglist)
 

a9f346b51c66b8063e75d32069293193d5d0b7e4
gperez2222 <student@bchd.a87c3ffd-bd40-4a64-8399-c05145c40292>
2023-07-12 21:40:16 +0000

    learning about functions

--- broken01/ifixed_new.py
+++ broken01/ifixed_new.py
@@ -1,45 +1,19 @@
 #!/usr/bin/env python3
 
+import re
+
 # A program that prompts a user for two operators and operation (plus or minus)
 # the program then shows the result.
 # The user may enter q to exit the program.
 
-calc1 = 0.0
-calc2 = 0.0
-operation = ""
+## Had to make the list global
 operators = ["+","-","*","/"]
 
-def arithmath(calc1,operation,calc2):
-
-    if operation == '+':
-        total = calc1 + calc2
-        print(f"\n {str(calc1)}  {operation}  {str(calc2)}  = {total}")
-    elif operation == '-':
-        total = calc1 - calc2
-        print(f"\n {str(calc1)}  {operation}  {str(calc2)}  = {total}")
-    elif operation == '*':
-        total = calc1 * calc2
-        print(f"\n {str(calc1)}  {operation}  {str(calc2)}  = {total}")
-    elif operation == '/':
-        total = calc1 / calc2
-        print(f"\n {str(calc1)}  {operation}  {str(calc2)}  = {total}")
-    else:
-        print("\n Not a valid entry. Restarting...")
-
-def raw_input():
-    value = input()
-
-    if value.isalpha():
-        return "q"
-    elif value.isdigit():
-        return float(value)
-    elif value in operators:
-        return value
-    else: 
-        return "q"
-
 def main():
-    while (calc1 != "q" and calc2 != "q"):
+    calc1 = 0.0
+    calc2 = 0.0
+    operation = ""
+    while calc1 != "q" and calc2 != "q":
 
             print("\nWhat is the first operator? Or, enter q to quit: ", end="")
             calc1 = raw_input()
@@ -59,8 +33,48 @@ def main():
             if operation in operators and calc1 != "q" and calc2 != "q":
                     arithmath(float(calc1),str(operation),float(calc2))
 
+def arithmath(calc1,operation,calc2):
+
+    if operation == '+':
+        total = float(calc1) + float(calc2)
+        print(f"\n {str(calc1)}  {operation}  {str(calc2)}  = {total}")
+    elif operation == '-':
+        total = float(calc1) - float(calc2)
+        print(f"\n {str(calc1)}  {operation}  {str(calc2)}  = {total}")
+    elif operation == '*':
+        total = float(calc1) * float(calc2)
+        print(f"\n {str(calc1)}  {operation}  {str(calc2)}  = {total}")
+    elif operation == '/':
+        total = float(calc1) / float(calc2)
+        print(f"\n {str(calc1)}  {operation}  {str(calc2)}  = {total}")
+    else:
+        print("\n Not a valid entry. Restarting...")
+
+    return None
+
+def isfloat(value):
+    # regular expression pattern to match valid float value
+    pattern = r"^[-+]?[0-9]*\.?[0-9]+$"
+
+    # Use re.match to check if the string matches the string pattern in value and returns a value object if there is a regular expression match, else None
+    checkformatch = re.match(pattern, value)
+
+    # Convert the match object to a boolean value and will eeturn True if there is a match, else False
+    return bool(checkformatch)
+
+def raw_input():
+    value = input()
+
+    if value.isalpha():
+        return "q"
+    elif value.isdigit() or isfloat(value):
+        return float(value)
+    elif value in operators:
+        return value
+    else: 
+        return "q"
+
 """ Script starts here"""
 
 if __name__ == "__main__":
     main()
-

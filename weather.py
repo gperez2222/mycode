1a524adf09046345e877585e8bead4d744a97e5c
gperez2222 <student@bchd.a87c3ffd-bd40-4a64-8399-c05145c40292>
2023-07-14 02:38:10 +0000

    Learning about open API - weather

--- weather.py
+++ weather.py
@@ -13,14 +13,23 @@ api_key = "6c8908cb4b9e383b4af05947cde8d465"
 # base_url variable to store url
 base_url = "http://api.openweathermap.org/data/2.5/weather?"
 
+default_city_name = "Cliffside Park"
+
 # Give city name
-city_name = input("Enter City Name - for ex: Cliffside Park:  ")
+city_name = input("Enter City Name or \"q\" for quit, for ex: [ Cliffside Park (def)]:  ")
+
+if "q" in city_name:
+    print(f"Exiting")
+    exit()
+
+if len(city_name) == 0:
+    city_name = default_city_name
 
 # compose the complete_url variable 
 #complete_url = base_url + "appid=" + api_key + "&q=" + city_name
 complete_url = f"{base_url}appid={api_key}&q={city_name}&units=imperial"
 
-##print(complete_url)
+#print(f"\nSearching weather for \"{city_name}\" using URL:{complete_url}\n")
 
 # get method of requests module
 # return response object
@@ -29,49 +38,39 @@ response = requests.get(complete_url)
 # json method of response object
 # convert json format data into
 # python format data
-x = response.json()
+json_output = response.json()
 
-##pprint(x)
+##pprint(json_output)
 
-# Now x contains list of nested dictionaries
-# Check the value of "cod" key is equal to
-# "404", means city is found otherwise,
-# city is not found
+# Now json_output contains list of nested dictionaries, check the value of "cod" key is equal to "404", 
+# means city is found otherwise city is not found
 
-if x["cod"] != "404":
+if json_output["cod"] != "404":
 
-	# store the value of "main"
-	# key in variable y
-	y = x["main"]
+	# store the value of "main" key in variable mainpage
+    mainpage = json_output["main"]
 
 	# store the value corresponding
-	# to the "temp" key of y
-	current_temperature = y["temp"]
+	# to the "temp" key of mainpage
+    current_temperature = mainpage["temp"]
 
 	# store the value corresponding
-	# to the "pressure" key of y
-	current_pressure = y["pressure"]
+	# to the "pressure" key of mainpage
+    current_pressure = mainpage["pressure"]
 
 	# store the value corresponding
-	# to the "humidity" key of y
-	current_humidity = y["humidity"]
+	# to the "humidity" key of mainpage
+    current_humidity = mainpage["humidity"]
 
-	# store the value of "weather"
-	# key in variable z
-	z = x["weather"]
+	# store the value of the "weather" key in variable "weather"
+    weather = json_output["weather"]
 
-	# store the value corresponding
-	# to the "description" key at
-	# the 0th index of z
-	weather_description = z[0]["description"]
+	# store the value corresponding to the "description" key at the 0th index of weather
+    weather_description = weather[0]["description"]
 
-	# print following values
-	print(f" Temperature (in far unit) = {str(current_temperature)}")
-	print(f" atmospheric pressure (in hPa unit) = {str(current_pressure)}")
-	print(f" humidity (in percentage) = {str(current_humidity)}")
-	print(f" description = {str(weather_description)}")
+    # print following values
+    print(f"{city_name}: {weather_description}, Temp: {current_temperature} Far: Hum: {current_humidity}%")
 
 else:
-	print(" City Not Found ")
-
+    print(f" {city_name} Not Found ")
 
